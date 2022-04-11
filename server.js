43441d00f52650c93a21b3fb72bb75c2fd5266bc
MariaAbritta <eduardaabritta@gmail.com>
2022-04-11 10:14:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conectei a base de dados');
+    app.emit('Pronto');
+  })
+  .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.on("Pronto", () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+});
+
+
